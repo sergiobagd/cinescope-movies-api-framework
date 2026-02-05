61ad86a0b8efd94b981a3edca743985d0baf7f3d
sergiobagd <ssergioffx@gmail.com>
2026-02-05 13:48:02 +0500

    chore: update DB and mock training files

--- practice/test_db_api.py
+++ practice/test_db_api.py
@@ -1,10 +1,10 @@
 import datetime
-
+import allure
 from db_models.account_transaction import AccountTransactionTemplate
 import pytest
-
 from db_models.movie import MovieDBModel
 from utils.data_generator import DataGenerator
+
 
 def add_movie_to_db(movie_id, db_helper):
     created_test_movie = DataGenerator.generate_movie_data()
@@ -13,115 +13,74 @@ def add_movie_to_db(movie_id, db_helper):
     db_helper.db_session.add(movie)
     db_helper.db_session.commit()
 
-def transfer_money(session, from_account, to_account, amount):
-    # Example of func of transaction
-    # Let's imagine it's written on the side of tested service
-    # And calling method "transfer_money" we like do request in api_manager.movies_api.transfer_money
-    """
-    Transfers money from one account to other
-    :param session: Session SQLAlchemy
-    :param from_account: ID of account where we take away money from
-    :param to_account: ID of account where we add money to
-    :param amount: Sum of transaction money
-    """
-    # Get accounts
-    from_account = session.query(AccountTransactionTemplate).filter_by(user=from_account).one()
-    to_account = session.query(AccountTransactionTemplate).filter_by(user=to_account).one()
-
-    # Check that account has enough money
-    if from_account.balance < amount:
-        raise ValueError("Not enough money on account")
-
-    # Making transaction
-    from_account.balance -= amount
-    to_account.balance += amount
-
-    # Save changes in DB
-    session.commit()
-
 def test_db_requests(super_admin, db_helper, created_test_user):
     assert created_test_user == db_helper.get_user_by_id(created_test_user.id)
     assert db_helper.user_exists_by_email("api1@gmail.com")
 
-def test_accounts_success_transaction_template(db_session):
-    # Create new data in DB
-    stan = AccountTransactionTemplate(user="Stan_Test10", balance=1000)
-    bob = AccountTransactionTemplate(user="Bob_Test15", balance=500)
+@allure.epic("Testing transactions")
+@allure.feature("Testing transactions between accounts")
+class TestAccountTransactionTemplate:
+    @allure.story("Validity of money transfer between two accounts")
+    @allure.description("""
+    This test checks validity of money transfer between two accounts.
+    Steps:
+    1. Creating two accounts: Stan and Bob.
+    2. Transfer 200 rubles from Stan to Bob.
+    3. Check that account's balances have changed
+    4. Clean up test data""")
+    @allure.severity(allure.severity_level.CRITICAL)
+    @allure.label("qa_name", "Ivan Petrovich")
+    @allure.title("Testing money transfer between two accounts")
+    def test_accounts_success_transaction_template(self, db_session):
+        with allure.step("Creating test data in DB: accounts Stan and Bob"):
+            stan = AccountTransactionTemplate(user="Stan_Test10", balance=1000)
+            bob = AccountTransactionTemplate(user="Bob_Test15", balance=500)
+            db_session.add_all([stan, bob])
+            db_session.commit()
 
-    # Add new data (users) into session
-    db_session.add_all([stan, bob])
+        @allure.step("Function of money transfer: transfer money")
+        @allure.description("""
+            Func that executes transaction
+            Imitation of calling func on the tested service side
+            And calling method "transfer_money" we like do request in api_manager.movies_api.transfer_money 
+            """)
+        def transfer_money(session, from_account, to_account, amount):
+            with allure.step("Get accounts"):
+                from_account = session.query(AccountTransactionTemplate).filter_by(user=from_account).one()
+                to_account = session.query(AccountTransactionTemplate).filter_by(user=to_account).one()
 
-    # Commit our changes in DB
-    db_session.commit()
+            with allure.step("Check that account has enough money"):
+                if from_account.balance < amount:
+                    raise ValueError("Not enough money on account")
 
-    assert stan.balance == 1000
-    assert bob.balance == 500
+            with allure.step("Making transaction"):
+                from_account.balance -= amount
+                to_account.balance += amount
 
-    try:
-        # Make transaction 200 rubles from Stan to Bob
-        transfer_money(db_session, from_account=stan.user, to_account=bob.user, amount=200)
+            with allure.step("Save changes in DB"):
+                session.commit()
 
-        # Check that balances have changed
-        assert stan.balance == 800
-        assert bob.balance == 700
-    except Exception as e:
-        # If error occured, - rollback transaction
-        db_session.rollback() # undo all our changes
-        pytest.fail(f"Error while sending money: {e}")
-    finally:
-        # Delete data for testing drom DB
-        db_session.delete(stan)
-        db_session.delete(bob)
-        # Commit our changed in DB
-        db_session.commit()
+        with allure.step("Check initial balances"):
+            assert stan.balance == 1000
+            assert bob.balance == 500
 
-# def test_accounts_fail_transaction_template(db_session):
-#     # Create new data in DB
-#     stan = AccountTransactionTemplate(user="Stan_Test10", balance=150)
-#     bob = AccountTransactionTemplate(user="Bob_Test15", balance=500)
-#
-#     # Add new data (users) into session
-#     db_session.add_all([stan, bob])
-#
-#     # Commit our changes in DB
-#     db_session.commit()
-#
-#     assert stan.balance == 150
-#     assert bob.balance == 500
-#
-#     try:
-#         # Make transaction 200 rubles from Stan to Bob
-#         transfer_money(db_session, from_account=stan.user, to_account=bob.user, amount=200)
-#
-#         # Check that balances have not changed
-#         assert stan.balance == 150
-#         assert bob.balance == 500
-#     except Exception as e:
-#         # If error occured, - rollback transaction
-#         db_session.rollback() # undo all our changes
-#         pytest.fail(f"Error while sending money: {e}")
-#     finally:
-#         # Delete data for testing drom DB
-#         db_session.delete(stan)
-#         db_session.delete(bob)
-#         # Commit our changed in DB
-#         db_session.commit()
+        try:
+            with allure.step("Make transaction 200 rubles from Stan to Bob"):
+                transfer_money(db_session, from_account=stan.user, to_account=bob.user, amount=200)
 
-def test_delete_movie(super_admin, db_helper):
-    """Test for movie deletion"""
+            with allure.step("Check that balances have changed"):
+                assert stan.balance == 800
+                assert bob.balance == 700
 
-    movie_id = 56
+        except Exception as e:
+            with allure.step("If error occured, - rollback transaction"):
+                db_session.rollback()
 
-    if_movie_exists = db_helper.get_movie_by_id(f"{movie_id}")
+            pytest.fail(f"Error while sending money: {e}")
 
-    if if_movie_exists is not None:
-        delete_response = super_admin.api.movies_api.delete_movie(movie_id)
-        get_response = super_admin.api.movies_api.get_specific_movie(movie_id, expected_status=404)
-        assert db_helper.get_movie_by_id(f"{movie_id}") is None
-    else:
-        add_movie_to_db(movie_id, db_helper)
-        assert db_helper.get_movie_by_id(f"{movie_id}") is not None
-        delete_response = super_admin.api.movies_api.delete_movie(movie_id)
-        get_response = super_admin.api.movies_api.get_specific_movie(movie_id, expected_status=404)
-        assert db_helper.get_movie_by_id(f"{movie_id}") is None
+        finally:
+            with allure.step("Delete data for testing drom DB"):
+                db_session.delete(stan)
+                db_session.delete(bob)
+                db_session.commit()
 
--- tests/api/test_mock_services.py
+++ tests/api/test_mock_services.py
@@ -1,12 +1,15 @@
 import datetime
 import pytz
 import requests
+from pytest_mock import mocker
+from unittest.mock import Mock
 from constants.constants import BASE_URL, HEADERS, REGISTER_ENDPOINT, LOGIN_ENDPOINT
 from custom_requester.custom_requester import CustomRequester
 from clients.api_manager import ApiManager
 from constants.roles import Roles
 from constants.models import RegisterUserResponse, TestUser
 from pydantic import BaseModel, Field
+from practice.service_what_is_today import what_is_today
 
 
 # Model pydantic for response of server worldclockapi
@@ -54,8 +57,8 @@ class TestTodayIsHolidayServiceAPI:
     def test_what_is_today(self): # check if Fake service what_is_today works
         # Requesting current time from service worldclockap
         world_clock_response = get_worldclockapi_time()
-        what_is_today_response = requests.post("http://127.0.0.1:16002/what_is_today", data=DateTimeRequest(currentDateTime=world_clock_response.currentDateTime))
-
+        what_is_today_response = requests.post("http://127.0.0.1:16002/what_is_today",
+                                               data=DateTimeRequest(currentDateTime=world_clock_response.currentDateTime).model_dump_json())
 
         # Check status of response from tested service
         assert what_is_today_response.status_code == 200, "Remote service is not accessible"
@@ -63,3 +66,128 @@ class TestTodayIsHolidayServiceAPI:
         what_is_today_data = WhatIsTodayResponse(**what_is_today_response.json())
         # Validate response of tested service
         assert what_is_today_data.message == "Today there is no holiday in Russia"
+
+    def test_what_is_today_BY_MOCK(self, mocker):
+        # Create mock for function get_worldclockap_time
+        mocker.patch(
+            "test_mock_services.get_worldclockapi_time",
+            return_value=Mock(
+                currentDateTime="2025-01-01T00:00Z" # Mock date for returning holiday of New Year
+            )
+        )
+
+        # Execute body of previous test again
+        world_clock_response = get_worldclockapi_time() # = 2025-01-01T00:00Z
+
+        what_is_today_response = requests.post("http://127.0.0.1:16002/what_is_today",
+                                               data=DateTimeRequest(currentDateTime=world_clock_response.currentDateTime).model_dump_json())
+
+        # Check status of response of tested service
+        assert what_is_today_response.status_code == 200, "Remote service is not avaliable"
+
+        # Parse JSON response of tested service with using pydantic model
+        what_is_today_data = WhatIsTodayResponse(**what_is_today_response.json())
+
+        assert what_is_today_data.message == "Новый год", "Should be 'Новый год'!!!"
+
+    # Creating Stub for func get_worldclockapi_time()
+    def stub_get_wordclockapi_time(self):
+        class StubWorldClockResponse:
+            def __init__(self):
+                self.currentDateTime = "2025-05-09T00:00Z" # Fixed date for Stub
+        return StubWorldClockResponse()
+
+    # Test with using Stub
+    def test_what_is_today_BY_STUB(self, monkeypatch):
+        # Change real func get_worldclockapi_time on stub
+        monkeypatch.setattr("test_mock_services.get_worldclockapi_time", self.stub_get_wordclockapi_time)
+        # или же можем просто напрямую взять значение из Stub world_clock_response = stub_get_worldclockap_time()
+
+        # Execute body of previous test again
+        world_clock_response = get_worldclockapi_time() # Makes call Stub that returns "2025-05-09T00:00Z"
+
+        # Executing request to the tested service
+        what_is_today_response = requests.post("http://127.0.0.1:16002/what_is_today",
+                                               data=DateTimeRequest(currentDateTime=world_clock_response.currentDateTime).model_dump_json())
+
+        assert what_is_today_response.status_code == 200
+        what_is_today_data = WhatIsTodayResponse(**what_is_today_response.json())
+        assert what_is_today_data.message == "День Победы", "Should be 'День Победы'!!!"
+
+    def run_wiremock_worldclockapi_time(self):
+        # Launching WireMock server (if uses standalone, this step can be skipped)
+        """
+        docker run -it --rm -p 8080:8080 --name wiremock wiremock/wiremock:3.12.0
+        """
+        wiremock_url = "http://localhost:8080/__admin/mappings"
+        mapping = {
+            "request": {
+                "method": "GET",
+                "url": "/wire/mock/api/json/utc/now"  # Эмулируем запрос к worldclockapi
+            },
+            "response": {
+                "status": 200,
+                "body": '''{
+                            "$id": "1",
+                            "currentDateTime": "2025-03-08T00:00Z",
+                            "utcOffset": "00:00",
+                            "isDayLightSavingsTime": false,
+                            "dayOfTheWeek": "Wednesday",
+                            "timeZoneName": "UTC",
+                            "currentFileTime": 1324567890123,
+                            "ordinalDate": "2025-1",
+                            "serviceResponse": null
+                        }'''
+            }
+        }
+        response = requests.post(wiremock_url, json=mapping)
+        assert response.status_code == 201, "Couldn't set up wiremock"
+
+    def test_what_is_today_BY_WIREMOCK(self): # This test is like maximum based test
+        # Launch our wiremock server
+        self.run_wiremock_worldclockapi_time()
+
+        # Making request to WireMock (imitation of worldcloclapi)
+        world_clock_response = requests.get("http://localhost:8080/wire/mock/api/json/utc/now")
+        assert world_clock_response.status_code == 200, "Remote service is unavailable"
+        # Parse JSON response with pydantic model
+        current_date_time = WorldClockResponse(**world_clock_response.json()).currentDateTime
+
+        # Send request to tested service what_is_today
+        what_is_today_response = requests.post(
+            "http://127.0.0.1:16002/what_is_today",
+            data=DateTimeRequest(currentDateTime=current_date_time).model_dump_json()
+        )
+
+        assert what_is_today_response.status_code == 200, "Remote service is unavailable"
+        what_is_today_data = WhatIsTodayResponse(**what_is_today_response.json())
+        assert what_is_today_data.message == "Международный женский день", "8 марта же?"
+
+    # Func sending request to fake service worldclockapi to get current date
+    def get_fake_worldclockapi_time(self) -> WorldClockResponse:
+        # Executing GET request
+        response = requests.get("http://127.0.0.1:16001/fake/worldclockapi/api/json/utc/now")
+        assert response.status_code == 200, "Remote service is not available"
+        return WorldClockResponse(**response.json())
+
+    # Test fake worldclockapi service
+    def test_fake_worldclockapi(self): # check if fake service worldclockapi works
+        world_clock_response = self.get_fake_worldclockapi_time()
+        # Print current date and time
+        current_date_time = world_clock_response.currentDateTime
+        print(f"Current date and time: {current_date_time=}")
+
+        assert current_date_time == datetime.datetime.now(pytz.utc).strftime("%Y-%m-%dT%H:%MZ"), "Date isn't matching"
+
+    def test_fake_what_is_today(self): # check if service what_is_today works
+        world_clock_response = self.get_fake_worldclockapi_time()
+
+        what_is_today_response = requests.post("http://127.0.0.1:16002/what_is_today",
+                                               data=DateTimeRequest(currentDateTime=world_clock_response.currentDateTime).model_dump_json())
+
+        assert what_is_today_response.status_code == 200, "Remote service is not available"
+        what_is_today_data = WhatIsTodayResponse(**what_is_today_response.json())
+        assert what_is_today_data.message == "Today there is no holiday in Russia", "There is no holiday!!!"
+
+
+
